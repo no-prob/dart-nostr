0b2632de1a3ade0ba65fecb16ae4bf3d843c3032
Nicolas Pulido <npulido@zonehacks.net>
2023-04-21 15:41:48 -0700

    update

--- lib/src/message.dart
+++ lib/src/message.dart
@@ -17,7 +17,7 @@ class Message {
     switch (type) {
       case "EVENT":
         message = Event.deserialize(data);
-        if (message.kind == 4) message = EncryptedDirectMessage(message);
+        if (message.kind == 4) message = EncryptedDirectMessage.receive(message);
         break;
       case "REQ":
         message = Request.deserialize(data);
